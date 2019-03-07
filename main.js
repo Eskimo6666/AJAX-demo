fd48ca07f0c4a13c8483c194f80c37d2939a8837
Eskimo <1145400536@qq.com>
2019-03-07 11:06:54 +0800

    JSON替换xml

---
+++ main.js
@@ -0,0 +1,22 @@
+myButton.addEventListener('click',(e)=>{
+    let request = new XMLHttpRequest()
+    request.onreadystatechange = ()=>{
+        if(request.readyState === 4){
+            console.log('请求响应都完毕了')
+            if(request.status >= 200&& request.status < 300){
+                console.log('说明请求成功了')
+                console.log(request.responseText)
+                let string = request.responseText
+                let object = window.JSON.parse(string)
+                console.log(object)
+                console.log(object.note)
+                console.log(object.note.from)
+            }else if(request.status >= 400){
+                console.log('说明请求失败了')
+            }
+        }
+    }
+    request.open('POST','/xxx')
+   
+    request.send()
+})
