bb97080837eec0f390c341b2d9976b0a33c97a4b
Eskimo <1145400536@qq.com>
2019-03-08 10:37:15 +0800

    jQuery 封装 AJAX

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
-myButton.addEventListener('click',(e)=>{
+window.jQuery = function(){
+    let nodes ={}
+    nodes.addClass =function(){}
+    nodes.html = function(){}
+    return nodes
+}
+
+window.jQuery.ajax = function(url,method, body,successFn ,failFn){
     let request = new XMLHttpRequest()
-    request.open('get','http://jack.com:8002/xxx')
-    request.send()
+    request.open(method,url) 
+    request.send(body)
     request.onreadystatechange = ()=>{
         if(request.readyState === 4){
-            console.log('请求响应都完毕了')
             if(request.status >= 200&& request.status < 300){
-                console.log('说明请求成功了')
-                console.log(request.responseText)
-                let string = request.responseText
-                let object = window.JSON.parse(string)
-                console.log(object)
-                console.log(object.note)
-                console.log(object.note.from)
+                successFn.call(undefined,request.responseText)
             }else if(request.status >= 400){
-                console.log('说明请求失败了')
+                failFn.call(undefined, request)
             }
         }
     }
-   
+}
+
+
+myButton.addEventListener('click',(e)=>{
+   window.jQuery.ajax('/xxx', 'post', 'a=1&b=2', (responseText)=>{console.log(1)} ,(rquest)=>{console.log(2)})
 })
