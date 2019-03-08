478ac93deab242ba5856b3a9d9bc3c5640060f38
Eskimo <1145400536@qq.com>
2019-03-08 13:48:53 +0800

    添加Promise

--- main.js
+++ main.js
@@ -6,50 +6,46 @@ window.jQuery = function () {
 }
 
 window.jQuery.ajax = function (options) {
-    let url = options.url
-    let method = options.method
-    let successFn = options.successFn
-    let failFn = options.failFn
-    let body = options.body
-    let headers = options.headers
-
-
-    let request = new XMLHttpRequest()
-    request.open(method, url)
-    for(let key in headers){
-        let value = headers[key]
-        request.setRequestHeader(key,value)
-    }
-    request.send(body)
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                successFn.call(undefined, request.responseText)
-            } else if (request.status >= 400) {
-                failFn.call(undefined, request)
+    return new Promise(function(resolve, reject){
+        let url = options.url
+        let method = options.method
+       
+        let body = options.body
+        let headers = options.headers
+    
+    
+        let request = new XMLHttpRequest()
+        request.open(method, url)
+        for(let key in headers){
+            let value = headers[key]
+            request.setRequestHeader(key,value)
+        }
+        request.onreadystatechange = () => {
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 300) {
+                    resolve.call(undefined, request.responseText)
+                } else if (request.status >= 400) {
+                    reject.call(undefined, request)
+                }
             }
         }
-    }
+        request.send(body)
+    })
+    
 }
 function f1(responseText){}
 function f2(responseText){}
 
 myButton.addEventListener('click', (e) => {
     window.jQuery.ajax({
-        url: '/gg',
+        url: '/xxx',
         method: 'get',
         headers:{
             'content-type':'application/x-www-form-urlencoded',
             'jack':20
-        },
-        successFn: (x) => {
-            console.log(x)
-            f1.call(undefined,x)
-            f2.call(undefined,x)
-        },
-        failFn: (x) => {
-            console.log(x)
-            console.log(x.status)
         }
-    })
+    }).then(
+        (text)=>{console.log(text)},
+        (request)=>{console.log(request)}
+    )
 })
